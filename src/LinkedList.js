da7f9b690af5adbbf6a5fd202f88a56c7a47bc91
LintonRobinson <40444726+LintonRobinson@users.noreply.github.com>
2025-08-16 06:55:37 -0400

    Function to prepend node to linked list

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -3,12 +3,24 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+    
+    prependNode(data) {
+        if (this.head === null) {
+            this.head = new Node();
+            this.head.value = data;
+            return 
+        }; 
+
+        let prependedNode = new Node();
+        prependedNode.value = data;
+        prependedNode.nextNode = this.head;
+        this.head = prependedNode;
+    };
 
     appendNode(data) {
         if (this.head === null) {
             this.head = new Node();
             this.head.value = data;
-            this.head.nextNode = null;
             return 
         }; 
 
@@ -18,6 +30,38 @@ class LinkedList {
         currentNode.nextNode.value = data;
 
     };
+
+    
+
+    getsize() {
+        if (this.head === null) throw Error('Link list is empty.');
+        let total = 0;
+        let currentNode = this.head
+        while (currentNode.nextNode != null) {
+            currentNode = currentNode.nextNode;
+            total++;    
+        }; 
+        
+        total++;
+        return total;
+    };
+
+
+    getHead() {
+        if (this.head === null) throw Error('Link list is empty.');
+        return this.head;
+    };
+
+    getTail() {
+        if (this.head === null) throw Error('Link list is empty.');
+        
+        let currentNode = this.head;
+        while (currentNode.nextNode != null) currentNode = currentNode.nextNode;
+
+        return currentNode;
+    };
+
+
 };
 
 export default LinkedList
--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+import util from 'util';
+util.inspect.defaultOptions.depth = null;
+util.inspect.defaultOptions.colors = true;
 // import "./style.css"
 import LinkedList from "./LinkedList.js"
 
 const myFirstList = new LinkedList();
 
 
-myFirstList.appendNode("My first link list node. Ya mama")
+myFirstList.appendNode("My first link list node. Ya mama");
 
-myFirstList.appendNode("Second")
+myFirstList.appendNode("Second");
+
+myFirstList.prependNode("No wait, this on is the first");
+
+
+
+
 
 console.log(myFirstList);
+
+console.log('This is the number of linked list', myFirstList.getsize());
+
+console.log('This is the head of the linked list', myFirstList.getHead());
+
+console.log('This is the head of the linked list', myFirstList.getHead());
